05e6eefed2e53be7f83e7ee2ae0b064478b15461
fernahh <fernahh@gmail.com>
2018-08-09 11:30:32 -0300

    Create alert component

---
+++ src/javascripts/components/alert/alert.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '../button/button'
+
+function Alert(props) {
+  return (
+    <div className="alert">
+      <p className="alert__message">{props.message}</p>
+      <Button onClick={props.retryAction}>Try Again</Button>
+    </div>
+  )
+}
+
+Alert.propTypes = {
+  message: PropTypes.string.isRequired,
+  retryAction: PropTypes.func.isRequired
+}
+
+export default Alert
---
+++ src/javascripts/components/alert/alert.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import sinon from 'sinon'
+import Alert from './alert'
+
+it('should render element with alert class', () => {
+  const wrapper = shallow(<Alert message="Foobar" retryAction={() => {}} />)
+  expect(wrapper.hasClass('alert')).toEqual(true)
+})
+
+it('should render a paragraph with alert message', () => {
+  const wrapper = shallow(<Alert message="Foobar" retryAction={() => {}} />)
+  const message = wrapper.find('.alert__message').text()
+  expect(message).toEqual('Foobar')
+})
+
+it('should execute retry action', () => {
+  const action = sinon.spy()
+  const wrapper = shallow(<Alert message="Foobar" retryAction={action} />)
+  wrapper.find('Button').simulate('click')
+  expect(action.calledOnce).toEqual(true)
+})
