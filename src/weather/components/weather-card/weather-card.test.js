7a564bf739971f8fdd87d465091969540d083283
Luiz Fernando Rodrigues <fernahh@gmail.com>
2018-08-30 17:01:47 -0300

    Replace DOM tests by snapshot testing at weather card

--- src/weather/components/weather-card/weather-card.test.js
+++ src/weather/components/weather-card/weather-card.test.js
@@ -6,48 +6,41 @@ import date from '../../../services/date/date'
 jest.mock('../../../services/date/date')
 
 describe('<WeatherCard />', () => {
-  it('should set cold weather temperature', () => {
+  it('set cold weather temperature', () => {
     const weather = { temperature: 5 }
     const wrapper = shallow(<WeatherCard weather={weather} />)
-    const coldTemperature = '.weather-card__temperature--cold'
-    expect(wrapper.find(coldTemperature).length).toBeTruthy()
+    expect(wrapper).toMatchSnapshot()
   })
 
-  it('should set normal weather temperature', () => {
+  it('set normal weather temperature', () => {
     const weather = { temperature: 24 }
     const wrapper = shallow(<WeatherCard weather={weather} />)
-    const coldTemperature = '.weather-card__temperature--normal'
-    expect(wrapper.find(coldTemperature).length).toBeTruthy()    
+    expect(wrapper).toMatchSnapshot()
   }) 
 
-  it('should set hot weather temperature', () => {
+  it('set hot weather temperature', () => {
     const weather = { temperature: 26 }
     const wrapper = shallow(<WeatherCard weather={weather} />)
-    const coldTemperature = '.weather-card__temperature--hot'
-    expect(wrapper.find(coldTemperature).length).toBeTruthy()    
+    expect(wrapper).toMatchSnapshot()
   }) 
 
-  it('should show details', () => {
+  it('show details', () => {
     const weather = { temperature: 26 }
     const wrapper = shallow(<WeatherCard weather={weather} showDetails={true} />)
-    const detailsSelector = '.weather-card__details'
-    expect(wrapper.find(detailsSelector).length).toBeTruthy()
-
+    expect(wrapper).toMatchSnapshot()
   })
 
-  it('should not show details', () => {
+  it('do not show details', () => {
     const weather = { temperature: 26 }
     const wrapper = shallow(<WeatherCard weather={weather} />)
-    const detailsSelector = '.weather-card__details'
-    expect(wrapper.find(detailsSelector).length).toBeFalsy()
+    expect(wrapper).toMatchSnapshot()
   })
 
-  it('should render formatted update at time', () => {
+  it('render formatted update at time', () => {
     const weather = { updatedAt: 1535636728998 }
     const updatedAtFormatted = '2018-08-30 10:20'
     date.formatTimestamp.mockReturnValue(updatedAtFormatted)
     const wrapper = shallow(<WeatherCard weather={weather} />)
-    const formattedDateSelector = '.weather-card__update-at time'
 
     expect(date.formatTimestamp).toBeCalledWith({
       timestamp: weather.updatedAt,
@@ -57,6 +50,6 @@ describe('<WeatherCard />', () => {
       timestamp: weather.updatedAt,
       formatType: 'hh:mm:ss A'
     })
-    expect(wrapper.find(formattedDateSelector).contains(updatedAtFormatted)).toEqual(true)
+    expect(wrapper).toMatchSnapshot()
   })
 })
