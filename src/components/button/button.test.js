2bd41b346d311fb4d92a697151cdd87f2cf09db3
Luiz Fernando Rodrigues <fernahh@gmail.com>
2018-08-30 16:34:24 -0300

    Refactor button unit tests

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -2,26 +2,21 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Button from './button'
 
-it('should render element with button class', () => {
-  const wrapper = shallow(<Button />)
-  expect(wrapper.hasClass('button')).toEqual(true)
-})
-
-it('should execute function from onClick', () => {
-  const onButtonClickSpy = jest.fn()
-  const wrapper = shallow(<Button onClick={onButtonClickSpy} />)
-  wrapper.find('button').simulate('click')
-  expect(onButtonClickSpy).toBeCalled()
-})
-
-it('should render button with submit type', () => {
-  const wrapper = shallow(<Button type="submit" />)
-  const type = wrapper.find('button').prop('type')
-  expect(type).toEqual('submit')
-})
-
-it('should render button type as button when type is not present', () => {
-  const wrapper = shallow(<Button />)
-  const type = wrapper.find('button').prop('type')
-  expect(type).toEqual('button')
+describe('<Button />', () => {
+  it('render correctly', () => {
+    const wrapper = shallow(<Button />)
+    expect(wrapper).toMatchSnapshot()
+  })
+  
+  it('execute function from onClick', () => {
+    const onButtonClickSpy = jest.fn()
+    const wrapper = shallow(<Button onClick={onButtonClickSpy} />)
+    wrapper.find('button').simulate('click')
+    expect(onButtonClickSpy).toBeCalled()
+  })
+  
+  it('render button with submit type', () => {
+    const wrapper = shallow(<Button type="submit" />)
+    expect(wrapper).toMatchSnapshot()
+  })  
 })
