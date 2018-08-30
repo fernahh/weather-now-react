1ad4eb0087a41c4480487169688fd782c1364a30
Luiz Fernando Rodrigues <fernahh@gmail.com>
2018-08-30 16:42:52 -0300

    Add topbar snapshot testing

--- src/components/topbar/topbar.test.js
+++ src/components/topbar/topbar.test.js
@@ -3,12 +3,7 @@ import { shallow } from 'enzyme'
 import Topbar from './topbar'
 import Logo from '../logo/logo'
 
-it('should render element with topbar class', () => {
+it('render correctly', () => {
   const wrapper = shallow(<Topbar />)
-  expect(wrapper.hasClass('topbar')).toEqual(true)
-})
-
-it('should contains Logo component', () => {
-  const wrapper = shallow(<Topbar />)
-  expect(wrapper.contains(<Logo />)).toEqual(true)
+  expect(wrapper).toMatchSnapshot()
 })
