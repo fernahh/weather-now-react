da39121375573bd5698693c3dcc051e0c0243574
Luiz Fernando Rodrigues <fernahh@gmail.com>
2018-08-30 16:30:41 -0300

    Refactor alert component test

--- src/components/alert/alert.test.js
+++ src/components/alert/alert.test.js
@@ -2,20 +2,16 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Alert from './alert'
 
-it('should render element with alert class', () => {
-  const wrapper = shallow(<Alert message="Foobar" retryAction={() => {}} />)
-  expect(wrapper.hasClass('alert')).toEqual(true)
-})
-
-it('should render a paragraph with alert message', () => {
-  const wrapper = shallow(<Alert message="Foobar" retryAction={() => {}} />)
-  const message = wrapper.find('.alert__message').text()
-  expect(message).toEqual('Foobar')
-})
-
-it('should execute retry action', () => {
+describe('<Alert />', () => {
   const action = jest.fn()
   const wrapper = shallow(<Alert message="Foobar" retryAction={action} />)
-  wrapper.find('Button').simulate('click')
-  expect(action).toBeCalled()
+  
+  it('render correctly', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+  
+  it('execute retry action', () => {
+    wrapper.find('Button').simulate('click')
+    expect(action).toBeCalled()
+  })
 })
